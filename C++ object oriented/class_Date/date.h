1359c43beefa3ebab2e8a9cedf6d45916233839a
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-03-10 19:45:55 +0100

    Update date.h

--- C++ object oriented/class_Date/date.h
+++ C++ object oriented/class_Date/date.h
@@ -10,17 +10,21 @@ private:
 	bool format;
 	bool ifLeep() const;
 	
-	friend ostream& operator<<(ostream& out, const Date& d);
-	friend istream& operator>>(istream& in, Date& d);
+	friend ostream& operator<<(ostream &, const Date&);
+	friend istream& operator>>(istream &, Date&);
 
 public:
-	Date(int day=1, int month=1, int year=2001);
+	Date(int day = 1, int month = 1, int year = 2001, bool format = true);
 	Date(const char* date);
 
-	bool& SetFormat(bool format);
-
-	
-	
+	void NextDay();
+	void PrevDay();
+	void SelectedDay(int days=7);
+	bool ifEqual(const Date& other) const;
+	friend bool ifLess(const Date& d1, const Date& d2);
+	friend bool ifMore(const Date& d1, const Date& d2);
+	friend int NuberOfDays(const Date& d1, const Date& d2);
+	bool& SetFormat(bool displayFormat = true);
 
 };
 
