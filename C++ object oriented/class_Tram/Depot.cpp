89d21d1f8414bd87ab980d431f310a4af15b5c98
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-03-19 23:55:52 +0100

    Update Depot.cpp

--- C++ object oriented/class_Tram/Depot.cpp
+++ C++ object oriented/class_Tram/Depot.cpp
@@ -1,4 +1,20 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include "Depot.h"
 
+Depot::Depot(const char* name) : num(0)
+{
+  strncpy(depot_id, name, DEPOT_ID_MAX);
+  for(int i=0; i<MAX_TRANS; i++){
+    atbase[0] = nullptr;
+  }
+}
 
+void Depot::operator+=(Tram&){
+}
+
+void Depot::send_first(){
+}
+
+int Depot::atBase(){
+return 0;
+}
