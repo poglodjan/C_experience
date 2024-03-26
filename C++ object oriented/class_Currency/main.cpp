e89db3c3a97b2dca4719733a7260ed7d92c38325
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-03-26 08:34:53 +0100

    Update main.cpp

--- C++ object oriented/class_Currency/main.cpp
+++ C++ object oriented/class_Currency/main.cpp
@@ -1,42 +1,30 @@
 #include <iostream>
 using namespace std;
-#include "l3.h"
-
+#include "l5.h"
 
 int main()
 {
-	int t = 2;
+	british_currency bc1;
+	british_currency bc2(2, 30, 30);
+
+	cout << bc1 << endl;
+	//bc2.set_type() = british_currency::currency_type::after;
+	british_currency::change_type();
+
+	cout << bc1 << endl;
+	cout << bc2 << endl;
+
+	//cout << test_conversion(british_currency(2));
 
 
-	british_currency bc;
-	bc.read();
-	bc.print();
-
-	bc = add(bc, bc);
-	bc.print();
+	bank_deposit bd("John Smith");
+	set_deposit(bd, bc2);
+	//bd.set_deposit(2, 2, 2);
+	//british_currency::set_type() = british_currency::currency_type::before;
+	cout << bd;
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
