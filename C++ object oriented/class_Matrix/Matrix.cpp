efef4e781e13f9c6734fd9a968b5af0c22886364
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-03-29 15:25:43 +0100

    Create Matrix.cpp

---
+++ C++ object oriented/class_Matrix/Matrix.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "Matrix.h"
+
+Matrix::Matrix() :coef(nullptr), size(0){}
+
+Matrix::Matrix(int _size) : coef(nullptr), size(0) {
+	size = _size;
+	coef = new (nothrow) int* [size];
+	for (int i = 0; i < size; i++) {
+		coef[i] = new (nothrow) int[size];
+		for (int j = 0; j < size; j++) {
+			coef[i][j] = 0;
+		}
+	}
+}
+
+Matrix::Matrix(int** m, int _size) : coef(nullptr), size(0)
+{
+	size = _size;
+	coef = new (nothrow) int* [size];
+	for (int i = 0; i < size; i++) {
+		coef[i] = new (nothrow) int[size];
+		for (int j = 0; j < size; j++) {
+			coef[i][j] = m[i][j];
+		}
+	}
+}
+
+Matrix::~Matrix() {
+	for (int i = 0; i < size; i++)
+	{
+		delete[] coef[i];
+	}
+	delete[] coef;
+}
+
+Matrix::Matrix(const Matrix& m) : coef(nullptr), size(0)
+{
+	size = m.size;
+	coef = new(nothrow) int* [size];
+	for (int i = 0; i < size; i++) {
+		coef[i] = new (nothrow) int[size];
+		for (int j = 0; j < size; j++) {
+			coef[i][j] = m.coef[i][j];
+		}
+	}
+}
+
+Matrix& Matrix::operator=(const Matrix& m)
+{
+	if (this == &m)
+		return *this;
+	if (size != m.size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			delete[] coef[i];
+		}
+		delete[] coef;
+
+		coef = new (nothrow) int* [m.size];
+		for (int i = 0; i < m.size; i++)
+		{
+			coef[i] = new (nothrow) int[m.size];
+		}
+
+		size = m.size;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				coef[i][j] = m.coef[i][j];
+			}
+		}
+		return *this;
+	}
+}
+
+ostream& operator<<(ostream &out, const Matrix &m){
+	out << "( ";
+	for (int i = 0; i < m.size; i++) {
+		if (i != 0)
+			out << "  ";
+		for (int j = 0; j < m.size; j++)
+		{
+			out << m.coef[i][j] << " ";
+		}
+		if(i!=m.size-1)
+			out << endl;
+	}
+	out << ")" << endl;
+	return out;
+}
