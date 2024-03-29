6bb3942bf2332404bfb77627b02e3c2b88d7d20c
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-03-29 12:20:02 +0100

    Update Library.cpp

--- C++ object oriented/class_Library/Library.cpp
+++ C++ object oriented/class_Library/Library.cpp
@@ -14,17 +14,60 @@ ostream& operator<<(ostream& out, const Library& l) {
 	return out;
 }
 
-void Library::operator+=(Book& b) {
+bool Library::operator+=(Book& b) {
 	if (book_count < MAX_BOOKS) {
-		books[book_count++] = &b;
+		books[book_count++] = new Book(b);
+		return true;
 	}
 	else {
 		std::cout << "Library is full, cannot add more trams." << endl;
+		return false;
 	}
 }
 
 // ===== STAGE  4 ===== 
 
+Book* Library::Rent(const char* bookTitle) {
+	for (int i = 0; i < book_count; i++) {
+		if (books[i]->MatchTitle(bookTitle)) {
+			Book* rentedBook = books[i];
+			
+			for (int j = i; j < book_count - 1; j++) {
+				books[j] = books[j + 1];
+			}
+			books[book_count - 1] = nullptr;
+			--book_count;
+			return rentedBook;
+		}
+	}
+	cout << "Title: " << bookTitle << " - no such book in the Library" << endl;
+	return nullptr;
+}
+
 // ===== STAGE  5 ===== 
 
+void Library::ListByAuthor(const char* athorName) {
+	int foundBooks = 0;
+	cout << "Author name: [" << athorName << "]" << endl;
+	for (int i = 0; i < book_count; i++) {
+		if (books[i]->MatchAuthor(athorName)) {
+			++foundBooks;
+			cout << "Book: " << *books[i] <<endl;
+		}
+	}
+	cout << "Found a total of " << foundBooks << " books." << endl;
+}
+
 // ===== STAGE  6 ===== 
+
+void Library::FindByPhrase(const char* Phrase) {
+	int foundBooks = 0;
+	cout << "Searching for Phrase: [" << Phrase << "]" << endl;
+	for (int i = 0; i < book_count; i++) {
+		if (books[i]->MatchPhrase(Phrase)) {
+			cout << "Book: " << *books[i] << endl;
+			foundBooks++;
+		}
+	}
+	cout << "Found a total of " << foundBooks << " books." << endl;
+}
