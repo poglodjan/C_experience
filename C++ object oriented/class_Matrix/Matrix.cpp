4935a8909a5984b476c8d0dc3aad25effaabddb0
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-04-23 11:30:02 +0200

    Update Matrix.cpp

--- C++ object oriented/class_Matrix/Matrix.cpp
+++ C++ object oriented/class_Matrix/Matrix.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
-#include "Matrix.h"
+using namespace std;
+
+class Matrix {
+	int** coef;
+	int size;
+
+public:
+	Matrix();
+	Matrix(int _size);
+	Matrix(int** m, int _size);
+	~Matrix();
+	Matrix(const Matrix& m);
+	Matrix& operator=(const Matrix& m);
+
+	friend ostream& operator<<(ostream& out, const Matrix& m);
+};
+
 
 Matrix::Matrix() :coef(nullptr), size(0){}
 
