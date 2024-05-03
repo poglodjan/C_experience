c702393c3ade8f1e671e2dfc6fe055e3a0790db6
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-05-03 21:18:32 +0200

    Update game.h

--- C++ object oriented/class_jackpot/game.h
+++ C++ object oriented/class_jackpot/game.h
@@ -9,11 +9,9 @@ class Game
 	int no_to_draw;
 	Balls drawing_balls;
 public:
-	// Part 4
 	Game(string _name, int _no_of_balls, int _no_to_draw);
 	friend ostream& operator<<(ostream&, const Game&);
-	// Part 5
-	Balls draw();
 
+	Balls draw();
 	string get_name() const { return name; }
 };
