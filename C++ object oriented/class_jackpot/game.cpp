f42e269b511a5db4040fcb867bd8f748f7fd462f
poglodjan <100129357+poglodjan@users.noreply.github.com>
2024-05-03 21:18:02 +0200

    Update game.cpp

--- C++ object oriented/class_jackpot/game.cpp
+++ C++ object oriented/class_jackpot/game.cpp
@@ -2,3 +2,25 @@
 using namespace std;
 #include "game.h"
 #include "balls.h"
+
+Game::Game(string _name, int _n_of_balls, int _no_to_draw) : 
+	name(_name), no_to_draw(_no_to_draw), drawing_balls(_n_of_balls, nullptr) {};
+
+ostream& operator<<(ostream& out, const Game& g)
+{
+	out << "Game: " << g.name << ", balls in game: " << endl << g.drawing_balls;
+	return out;
+}
+
+Balls Game::draw()
+{
+	int res = 0;
+	Balls result(0, nullptr);
+	for (int i = 0; i < no_to_draw; i++)
+	{
+		int random_pos = rand() % drawing_balls.balls_no();
+		if (!drawing_balls.draw(random_pos, res)) break;
+		else result += res;
+	}
+	return result;
+}
